a04742c170e9e9e62d89890b788784e050144a0e
Nuno Silva <nunojosersilva@gmail.com>
2017-04-13 18:09:59 +0100

    Update Coordenate.java

--- end-poo/Coordenate.java
+++ end-poo/Coordenate.java
@@ -1,26 +1,26 @@
-public  class Coordenates {
+public  class Coordenate {
   private int x;
   private int y;
 
 
 /*
-Constructors of class Coordenates.
+Constructors of class Coordenate.
 
 */
-  public Coordenates(int nx,int ny){
+  public Coordenate(int nx,int ny){
     this.x=nx;
     this.y=ny;
   }
 
-  public Coordenates(){
+  public Coordenate(){
     this.x=0;
     this.y=0;
   }
 
-public Coordenates(Coordenates ref){
-  this.x=ref.getx();
-  this.y=ref.gety();
-}
+  public Coordenate(Coordenate ref){
+    this.x=ref.getx();
+    this.y=ref.gety();
+  }
 
 //Getters e setters
 
@@ -33,16 +33,27 @@ public int gety(){
   return this.y;
 }
 
-public int setx (int nx){
+public void setx (int nx){
   this.x=nx;
 }
 
-public int sety (int ny){
+public void sety (int ny){
   this.y=ny;
 }
 
 //Clone
 
-public Coordenates clone (){
-  return Coordenates(this)
+public Coordenate clone (){
+  Coordenate res = new Coordenate(this);
+  return res;
+}
+
+//Equals
+
+public boolean equals (Object ref){
+  if (ref==this) return true;
+  if ((ref==null)|| (ref.getClass()!=this.getClass())) return false;
+  Coordenate l = (Coordenate) ref;
+  return ((l.getx() == this.x) && (l.gety() == this.y));
+  }
 }
