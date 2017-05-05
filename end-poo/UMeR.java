db9c65e26115dcb5cd5022c0cb25d77f961e8758
Daniel Martins <daniel.martins50@gmail.com>
2017-05-05 20:46:14 +0100

    Update UMeR classe

--- end-poo/UMeR.java
+++ end-poo/UMeR.java
@@ -5,29 +5,18 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+
+import java.util.Map;
+import java.util.HashMap;
+
 public class UMeR
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private HashMap<int, Client> clientList;
+    private HashMap<int, Driver> driverList;
+    private HashMap<String, ? extends Vehicle> vehicleList;
+    private HashMap<int, Trip> tripList;
 
-    /**
-     * Constructor for objects of class UMeR
-     */
-    public UMeR()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static UMeR(){
+        
     }
 }
