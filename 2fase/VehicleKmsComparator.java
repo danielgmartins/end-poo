43a4103a28648a77d45bb7f7c8220da23f8cab6f
Elisio Fernandes <elisioff@me.com>
2017-05-17 15:01:05 +0100

    Adds comparators

---
+++ 2fase/VehicleKmsComparator.java
@@ -0,0 +1,16 @@
+/**
+ * VehicleKmsComparator - Compares Vehicle objects by the number os total kilometers
+ *
+ * @author  a55617 Elisio Fernandes, a73175 Daniel Martins, a78879 Nuno Silva
+ * @version 12/04/2017
+ */
+
+import java.util.Comparator;
+public class VehicleKmsComparator implements Comparator <Vehicle> {
+     public int compare (Vehicle v1, Vehicle v2){
+         double kms1 = v1.getKmsTotal();
+         double kms2 = v2.getKmsTotal();
+
+
+     }
+ }
